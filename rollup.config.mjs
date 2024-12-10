a4cfd0f4257beed6a7c00ce96c3d019226ae4ea7
Kyle Angelo Galendez <chessurisme@gmail.com>
2024-12-10 19:08:21 +0800

    chore: add alias for bundler
    Uses @rollup/plugin-alias to map paths, as Rollup doesn't support aliases
    natively.

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,8 +1,21 @@
+import alias from '@rollup/plugin-alias';
+
 export default {
 	input: 'src/index.js',
 
 	output: {
 		file: 'dist/index.js',
 		format: 'esm'
-	}
+	},
+	plugins: [
+		alias({
+			entries: [
+				{ find: '@utilities', replacement: './src/utilities' },
+				{ find: '@icons', replacement: './src/icons' },
+				{ find: '@components', replacement: './src/components' },
+				{ find: '@pages', replacement: './src/pages' },
+				{ find: '@tests', replacement: './src/tests' }
+			]
+		})
+	]
 };
